f9a29ab2001b25e90ceb1a847ca3f747ad4d1bb0
Brianyuko <brianyuko45@gmail.com>
2020-06-20 14:09:21 +0700

    Set Attribute

---
+++ Attribute.js
@@ -0,0 +1,6 @@
+var img = document.getElementById('ImageLinux');
+        img.width = 600
+        img.setAttribute('class', 'background')
+
+        var input = document.getElementById('inputNama')
+        input.value = "Nama Lengkap"
