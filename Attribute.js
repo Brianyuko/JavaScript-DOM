206d794769d02bb867734b55ab890d3b2d2a8d7e
Brianyuko <brianyuko45@gmail.com>
2020-06-20 17:19:29 +0700

    Event JavaScript

--- Attribute.js
+++ Attribute.js
@@ -1,6 +1,41 @@
 var img = document.getElementById('ImageLinux');
-        img.width = 600
-        img.setAttribute('class', 'background')
+img.width = 400
 
-        var input = document.getElementById('inputNama')
-        input.value = "Nama Lengkap"
+// Option One
+//img.setAttribute('class', 'background')
+
+// Option Two
+img.className += ' Background';
+
+var input = document.getElementById('inputNama')
+input.value = "Nama Lengkap"
+
+input.style.border = '7px solid yellow';
+input.style.backgroundColor = 'pink';
+
+function animateIn(){
+    img.style.backgroundColor = 'blue'
+}
+function animateOut(){
+    img.style.backgroundColor = 'pink'
+}
+
+// EVENT 
+
+// Option One
+// Menambahkan attribut di tag, contohnya onClick, lalu diisi dengan function
+// function changeColor(){
+//     img.style.backgroundColor = 'blue'
+// }
+
+// Option Two
+// img.onmouseenter = function(){ animateIn()};
+
+
+// Option Three
+img.addEventListener('mouseenter', function(){
+     animateIn();
+ });
+img.addEventListener('mouseleave', function(){
+     animateOut();
+ })
